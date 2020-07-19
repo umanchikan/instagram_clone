495954ca16f5451dedefeee74b246c5b105d7357
chiakisaito <a.cop.p2p2@gmail.com>
2020-07-19 14:58:26 +0900

    お気に入り機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "sessions#new"
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
-  resources :favorites, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy, :index]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :pictures do
     collection do
